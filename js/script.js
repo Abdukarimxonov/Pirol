a5b1cb2083d5eadd6316779c304ce85bdfde22d1
Abdukarimxonov <92791349+Abdukarimxonov@users.noreply.github.com>
2022-02-09 13:48:25 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function openNav() {
+  document.getElementById("mySidenav").classList.toggle('active');
+  document.querySelector("body").classList.toggle('lock');
+  console.log("Hello world");
+};
+
+
+function change(x) {
+  x.classList.toggle("change");
+};
+/*-----------------------------*/
+
